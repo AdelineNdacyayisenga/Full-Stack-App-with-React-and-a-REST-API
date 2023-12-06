55607c681be308f805e9243dc1f7307ae4a51d07
Adeline Ndacyayisenga <ndacya.adeline@gmail.com>
2023-12-05 16:37:02 -0800

    Display the NotFound error

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,9 +1,10 @@
 const NotFound = () => {
-    <div>
-        <h1>Not Found</h1>
-        <p>Sorry! We couldn't find the page you are looking for</p>
-    </div>
-
+    return (
+        <div>
+            <h1>Not Found</h1>
+            <p>Sorry! We couldn't find the page you are looking for</p>
+        </div>
+    );
 }
 
 export default NotFound;
